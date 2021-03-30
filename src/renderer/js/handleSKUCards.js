c241cdd0337ffa707fbfba7bdc85e1036a2ba304
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2021-03-29 19:45:36 -0600

    Update handleSKUCards.js

--- src/renderer/js/handleSKUCards.js
+++ src/renderer/js/handleSKUCards.js
@@ -1,14 +1,20 @@
 
+/**
+ * Given the data for a card: creates a card with the revelent sku data. Then appends it to the parent DOM element.
+ * @param {*} cardData The data that will be needed to craft the card.
+ * @param {*} masterContainer The container that will now append the new card to.
+ */
  export function createProductCard(cardData, masterContainer) {
 
     let sku, stock, img, remaining, prodName;
     let cardBody = document.createElement('div')
     cardBody.classList = "card auto";
     
-    // Product name
+    // Product name and revelent styling
     prodName = document.createElement('h1')
     prodName.classList = 'product-name'
     prodName.innerText = cardData.name;
+
     // sku
     sku = document.createElement("h4")
     sku.classList = 'small-header sku'
@@ -33,15 +39,22 @@
     remaining.classList = 'small-header'
     remaining.innerText = `Amount in stock: ${cardData.item.quantityRemaining}`;
 
+    // apend new elems to the master element
 	cardBody.appendChild(prodName)
     cardBody.appendChild(stock)
     cardBody.appendChild(img)
     cardBody.appendChild(sku)
     cardBody.appendChild(remaining)
     
+    
     masterContainer.appendChild(cardBody);
 }
 
+/**
+ * Given a string representation of the sku this function will return true/false whether or not the sku is in a valid format.
+ * @param {string} sku The sku in string format. If sku is an integer then convert to a str.
+ * @returns boolean -> isSku in correct format
+ */
 export function checkSKU(sku) {
 	let passed = false;
 	// console.log(`Printing sku.length !== 8: ${sku.length !== 8}`)
@@ -51,6 +64,10 @@ export function checkSKU(sku) {
 	}
 }
 
+/**
+ * Shortens the length of the product and returns it.
+ * @param {string} name A string containing the name to be shortened.
+ */
 function shortenLenghth (name) {
     let shortName = name
 			.split(' ')
