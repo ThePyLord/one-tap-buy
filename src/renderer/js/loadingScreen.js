8f66a9c43efc4b591df89f6ac5f9333ed01898f9
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2021-03-29 22:49:48 -0600

    Added Some much needed comments and clarity inside the docs.

--- src/renderer/js/loadingScreen.js
+++ src/renderer/js/loadingScreen.js
@@ -1,18 +1,25 @@
 let loadingBar = document.getElementById("loading-bar")
 let percentageText = document.getElementById("loading-percent");
 
+// Variables needed for the loading sequence.
 let total_skus_to_load = 0;
 let loaded_count = 0;
 
-APP.on("getting-sku-init-data", (data) => {
+APP.once("getting-sku-init-data", (data) => {
     total_skus_to_load = data.length;
 });
 
-
+/**
+ * Updates the Starting loading screens percentage.
+ * @param {number} percentage 0.01 - 1
+ */
 function updateLoadingPercentage (percentage = .001) {
     console.log(`Loading ${(percentage * 100).toFixed(0)}%.`);
     if (percentage >= 1) {
+        
         APP.emit("loading-finished");
+        APP.removeListener("new-data-loaded") // remove the event listener so it clears up some memory allocation.
+
         document.getElementById("loading-screen").style.display = "none";
         document.getElementById("main").style.display = "block";
     } else {
@@ -23,7 +30,9 @@ function updateLoadingPercentage (percentage = .001) {
 }
 
 
+// whenever some data has been loaded and added to the dom during startup.
 APP.on("new-data-loaded", (e) => {
+    // loaded count is the count of all the elementsm retrieved from the initial startup sequence
     loaded_count += 1;
     let percentageLoadedCurrently = loaded_count / total_skus_to_load;
     updateLoadingPercentage(percentageLoadedCurrently);
--- src/renderer/js/renderer.js
+++ src/renderer/js/renderer.js
@@ -2,12 +2,19 @@ import {createProductCard, checkSKU} from "./handleSKUCards.js"; // handle dom w
 import {_PrototypingTools} from "./testing/Tools.js"; // testing library
 import {initSkusSettings} from "./storage/storage.js";
 
+// Needed Variables to handle adding skus to the dom.
 let renderedSkusList = []; // list of all skus that have been appended to the dom.
 let loadingFinished = false;
 
+
+// TESTING DATA /// REMOVE IN PRODUCTION
 _PrototypingTools.setTestSKUS(['14575596','14583636','15317229', '15178453','13444247','14425777', '15254466', '15299574', '15136919', '14583636', '14967578', '14934419', '14584169']); // run this then comment out line:)
 // _PrototypingTools.clearSkusStorage() used to clear storage. TESTING FUNCTION MAINLY
 
+
+/**
+ * Takes the products from storage and retrieves the product information data from bb.
+ */
 function initData() {
 
     initSkusSettings().then(config => {
@@ -21,19 +28,24 @@ function initData() {
     });
 }
 
+// get all the products and their corresponding data.
+// this will also initialize the sequence to the loading screen.
 initData()
 
 
+// 
 ipcRenderer.on('received-data', (event, data) => {
     
+    // handle adding the new data to the dom.
     if (data.length === 1 && !renderedSkusList.includes(data[0].item.SKU)) {
         renderedSkusList.push(data[0].item.SKU)
         let cardsContainer = document.getElementById('cards');
         createProductCard(data[0], cardsContainer);
     }
 
+
     // hendle loading animations with curent data
-    if (!loadingFinished) {
+    if (!loadingFinished) { // runs only if in the initial loading state.
         APP.emit("new-data-loaded");
     }
 
@@ -42,6 +54,7 @@ ipcRenderer.on('received-data', (event, data) => {
 //// skus: ['15317229', '15178453','13444247','14425777']
 
 
-APP.on("loading-finished", (e) => {
+// used to signifiy the loading sequence is finished.
+APP.once("loading-finished", (e) => {
     loadingFinished = true;
 });
